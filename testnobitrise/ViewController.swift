8874a102bbb996fdca89302ecd6449452cb483ed
shun-sasano <jumpjudo3303@gmail.com>
2018-11-25 11:02:12 +0900

    Initial Commit

---
+++ testnobitrise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testnobitrise
+//
+//  Created by 笹野駿 on 2018/11/25.
+//  Copyright © 2018 笹野駿. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
