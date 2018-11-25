3e7077774a5d865a034529e15dd566f3c6c5f9f6
shun-sasano <jumpjudo3303@gmail.com>
2018-11-25 11:11:54 +0900

    first commit

--- testnobitriseTests/testnobitriseTests.swift
+++ testnobitriseTests/testnobitriseTests.swift
@@ -23,6 +23,10 @@ class testnobitriseTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testGetText() {
+        XCTAssertEqual(get(text: "test"), "test")
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
